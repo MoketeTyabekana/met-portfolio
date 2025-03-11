2474548d860659bfaad27fcea57ecee7b4c3c13d
Mokete <ematyabekana@gmail.com>
2025-03-11 12:47:48 +0200

    updated about section; adjust padding for improved layout consistency

--- src/App.jsx
+++ src/App.jsx
@@ -126,7 +126,7 @@ const App = () => {
 
       {/* Sections */}
       <div className="">
-        <section id="about" className="h-screen  bg-gray-950 py-8 px-16">
+        <section id="about" className="h-screen  bg-gray-950  p-16">
           <div
             className="px-4 py-8 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100/20
  text-white  flex flex-col gap-4 hover:shadow-[0_0_10px_rgba(59,130,246,0.7)]"
