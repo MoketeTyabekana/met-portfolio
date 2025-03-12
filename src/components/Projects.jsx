756ea2425bc093ac29987e7cd50a35d58ed059fa
Mokete <ematyabekana@gmail.com>
2025-03-12 13:38:35 +0200

    enhanced Projects component layout and add new project entries

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,15 +10,17 @@ const Projects =()=>{
 
 <div className="px-4 py-8 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100/20 text-white  flex flex-col gap-4 hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] w-full">
   <h2 className="text-xl font-semibold">Development</h2>
-   <div className="flex gap-4">
+
+   <div className="flex flex-wrap justify-between gap-4">
+
    {projects.projects && projects.projects.length > 0 && projects.projects.map((project, index) => (
    
    <div
-     className=" text-center bg-white rounded-md text-gray-950 flex flex-col items-center justify-between gap-y-4 w-1/3 h-auto py-4 px-2"
+     className=" text-center bg-white rounded-md text-gray-950 flex flex-col items-center justify-between w-[250px] h-auto  p-2 hover:scale-105"
      key={index}
    >
-     <img src={project.image}alt="project image" />
-     <a href={project.link} className="text-sm"><span className="text-sm">{project.title}</span></a>
+     <img src={project.image} alt="project image"  className="rounded"/>
+     <a href={project.link} className="text-xl font-bold text-gray-800 "><span className="text-sm ">{project.title} </span></a>
    </div>
  ))}
    </div>
