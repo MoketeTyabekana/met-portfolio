3b9b75ba4ffa8c8ffe23b48da2086cf989d98e57
Mokete <ematyabekana@gmail.com>
2025-03-13 15:53:55 +0200

    refactor: add custom screen breakpoints to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ export default {
   ],
   theme: {
     extend: {
+      screen:{
+           'sm':{min: "319px", max: "479px" },
+           'md':{min: "768px", max: "1023px"},
+           'lg':{min: "1024px", max: "1279px"},
+      },
       boxShadow: {
         'glow': '0 0 10px rgba(120,150, 0, 0.5)',
       },
