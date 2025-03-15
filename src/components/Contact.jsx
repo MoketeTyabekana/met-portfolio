c8516a389583b86cf2ae32e64f33828300e590a1
Mokete <ematyabekana@gmail.com>
2025-03-15 14:21:46 +0200

    refactor: improve layout and responsiveness in Contact and Footer components for better user experience

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,42 +6,43 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="h-auto flex flex-col gap-10 bg-gray-950 text-white  p-16"
+      className="h-auto flex flex-col lg:gap-10 gap-4 bg-gray-950 text-white p-4 lg:p-16"
     >
-      <h2 className="text-3xl font-semibold">Contact</h2>
-      <div className=" flex justify-between items-center">
-        <div className="text-xl  bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col gap-4 hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] px-10 py-2">
+      <h2 className=" text-xl lg:text-3xl font-semibold">Contact</h2>
+
+      <div className=" lg:flex lg:justify-between flex-row justify-center   items-center">
+        <div className="lg:text-xl text-lg  bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col text-center gap-4 lg:hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] px-10 py-2">
           <h1>emtyabekana@gmail.com</h1>
         </div>
 
-        <div className="flex space-x-10 mt-8 ">
+        <div className="flex lg:space-x-10 justify-between mt-8 ">
           <a
             href="#"
-            className=" bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col gap-4 hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] p-2"
+            className=" bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col gap-4 lg:hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] p-2"
           >
             <FaLinkedinIn size={24} />
           </a>
           <a
             href="#"
-            className=" bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col gap-4 hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] p-2"
+            className=" bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col gap-4 lg:hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] p-2"
           >
             <FaGithub size={24} />
           </a>
           <a
             href="#"
-            className=" bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col gap-4 hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] p-2"
+            className=" bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col gap-4 lg:hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] p-2"
           >
             <FaFacebookF size={24} />
           </a>
           <a
             href="#"
-            className=" bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col gap-4 hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] p-2"
+            className=" bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col gap-4 lg:hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] p-2"
           >
             <FaTwitter size={24} />
           </a>
           <a
             href="#"
-            className=" bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col gap-4 hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] p-2"
+            className=" bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-800/20 text-white  flex flex-col gap-4 lg:hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] p-2"
           >
             <FaInstagram size={24} />
           </a>
