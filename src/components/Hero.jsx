5baf26400a87663777e719d193277231800213d5
Mokete <ematyabekana@gmail.com>
2025-03-15 13:43:53 +0200

    refactor: improve layout and text responsiveness in Hero component for better user experience

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,11 @@ const Hero =() =>{
         id="home"
         className="lg:h-full h-full grid lg:grid-cols-2 gap-4 items-center justify-centers bg-gray-900 text-white lg:p-16 px-6 pt-40"
       >
-        <div className="flex flex-col items-start ">
-          <h1 className="text-4xl font-bold mb-4">
+        <div className="flex flex-col lg:justify-start lg:text-start text-center">
+          <h1 className="lg:text-4xl text-2xl font-bold mb-4">
             <span className=" text-gray-700">MR.</span> MOKETE TYABEKANA
           </h1>
-          <p className="text-lg mb-8 max-w-3xl">
+          <p className="text-sm lg:text-xl mb-8 max-w-3xl">
             Great front-end development isn’t just about writing code—it’s about
             crafting seamless, intuitive experiences that users love. Every
             pixel, every interaction, and every animation tells a story—make it
@@ -28,7 +28,7 @@ const Hero =() =>{
          
          
          
-          <div className="flex space-x-10 mt-8">
+          <div className="flex space-x-10 mt-8 lg:w-fit w-full justify-center">
             <a href="#" className="text-white hover:text-gray-400">
               <FaFacebookF size={24} />
             </a>
@@ -46,7 +46,7 @@ const Hero =() =>{
             </a>
           </div>
         </div>
-        <div className="relative  flex lg:justify-end  p-10 lg:py-20">
+        <div className="relative  flex lg:justify-end justify-center  p-10 lg:py-20">
           <img
             src={mokete}
             alt="Profile"
