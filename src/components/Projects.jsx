e14126ba4b7cbd8b23bdf5625f539b85aa8a9910
Mokete <ematyabekana@gmail.com>
2025-03-17 18:34:11 +0200

    feat: add GitHub links to projects in development.json; enhance project accessibility style: adjust layout and spacing in Projects component; improve visual consistency

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import projects from "../data/development.json";
-import designs from "../data/designs.json"
-import { FaArrowRight,FaFigma,FaGithub } from "react-icons/fa";
+import designs from "../data/designs.json";
+import { FaArrowRight, FaFigma, FaGithub } from "react-icons/fa";
 
 const Projects = () => {
   return (
@@ -13,9 +13,11 @@ const Projects = () => {
 
       {/* Coding Projects */}
       <div className="px-4 py-8 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100/20 text-white  flex flex-col gap-4 lg:hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] w-full">
-        <h2 className="lg:text-3xl text-2x1 font-semibold">Font-End Development</h2>
+        <h2 className="lg:text-3xl text-2x1 font-semibold">
+          Font-End Development
+        </h2>
 
-        <div className="grid grid-cols-2 lg:grid-cols-4  lg:justify-between gap-2  lg:gap-6">
+        <div className="grid grid-cols-1 lg:grid-cols-4  lg:justify-between gap-2  lg:gap-6">
           {projects.projects &&
             projects.projects.length > 0 &&
             projects.projects.slice(0, 4).map((project, index) => (
@@ -28,16 +30,20 @@ const Projects = () => {
                   alt="project image"
                   className="rounded"
                 />
-                <div className="flex flex-row justify-center w-full mt-4">
-                  <a
-                    href={project.link}
-                    target="_blank"
-                    className="text-xl flex items-center justify-center  gap-2 "
-                  >
+                <div className="flex flex-row justify-between items-center w-full mt-4">
+                  <a href={project.link} className="flex justify-start gap-2 items-center">
                     <span className="text-sm lg:text-lg font-semibold text-gray-800">
                       {project.title}
-                    </span>{" "}
-                    <FaGithub size={16} className="text-gray-800" />
+                    </span>
+                    <span className="text-gray-800"><FaArrowRight /></span>
+                  </a>
+
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    className=" text-gray-800 cursor-pointer bg-gray-900 p-2 rounded-full animate-pulse"
+                  >
+                    <FaGithub size={16} className="text-gray-50" />
                   </a>
                 </div>
               </div>
@@ -62,40 +68,40 @@ lg:hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] w-full"
       >
         <h2 className="lg:text-3xl text-2x1 font-semibold">UI/UX Design</h2>
 
-        <div className="grid grid-cols-2 lg:grid-cols-4 lg:justify-between gap-2 lg:gap-6">
-  {designs.designs &&
-    designs.designs.length > 0 &&
-    designs.designs.slice(0, 4).map((design, index) => (
-      <div
-        className="text-center bg-gray-50 rounded-md text-gray-950 flex flex-col items-center h-auto p-2 lg:hover:scale-105 gap-4"
-        key={index}
-      >
-        {/* Image wrapper to control aspect ratio */}
-        <div className="w-full aspect-[5/3] rounded overflow-hidden">
-          <img
-            src={design.image}
-            alt="project image"
-            className="w-full h-full object-cover object-top"
-          />
-        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-4 lg:justify-between gap-2 lg:gap-6">
+          {designs.designs &&
+            designs.designs.length > 0 &&
+            designs.designs.slice(0, 4).map((design, index) => (
+              <div
+                className="text-center bg-gray-50 rounded-md text-gray-950 flex flex-col items-center h-auto p-2 lg:hover:scale-105 gap-4"
+                key={index}
+              >
+                {/* Image wrapper to control aspect ratio */}
+                <div className="w-full aspect-[5/3] rounded overflow-hidden">
+                  <img
+                    src={design.image}
+                    alt="project image"
+                    className="w-full h-full object-cover object-top"
+                  />
+                </div>
 
-        {/* Title link */}
-        <div className="flex flex-row justify-center items-center w-full">
-          <a
-            href={design.link}
-            target="_blank"
-            className="text-xl flex items-center justify-center gap-2"
-          >
-            <span className="text-sm lg:text-lg font-semibold text-gray-800">
-              {design.title}
-            </span>
-            <FaFigma size={16} className="text-gray-800" />
-          </a>
-        </div>
-      </div>
-    ))}
-</div>
+                {/* Title link */}
+                <div className="flex flex-row justify-between items-center w-full">
+                  <span className="text-sm lg:text-lg font-semibold text-gray-800">
+                    {design.title}
+                  </span>
 
+                  <a
+                    href={design.link}
+                    target="_blank"
+                    className=" text-gray-800 cursor-pointer bg-gray-900 p-2 rounded-full animate-pulse"
+                  >
+                    <FaFigma size={16} className="text-gray-50" />
+                  </a>
+                </div>
+              </div>
+            ))}
+        </div>
 
         {/* A condition statement to activate the view mmore button only if the array has more than 4 projects in the list */}
         {designs.designs && designs.designs.length > 4 && (
@@ -107,7 +113,6 @@ lg:hover:shadow-[0_0_10px_rgba(59,130,246,0.7)] w-full"
           </a>
         )}
       </div>
-
     </section>
   );
 };
