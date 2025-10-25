07b66a8bed5c055d7e0362490d08fc7ce92173fc
Mokete <ematyabekana@gmail.com>
2025-10-25 14:17:09 +0200

    Refactor skills data structure to use an array of objects with icons and links directly in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,33 +17,26 @@ import {
   SiSass
 } from "react-icons/si";
 
-import { skills as skillsData } from "../data/skills";
-
-const iconMap = {
-  SiHtml5: <SiHtml5 />,
-  SiCss3: <SiCss3 />,
-  SiJavascript: <SiJavascript />,
-  SiReact: <SiReact />,
-  SiTailwindcss: <SiTailwindcss />,
-  SiBootstrap: <SiBootstrap />,
-  SiGit: <SiGit />,
-  SiFigma: <SiFigma />,
-  SiAdobephotoshop: <SiAdobephotoshop />,
-  SiAdobexd: <SiAdobexd />,
-  SiAdobeillustrator: <SiAdobeillustrator />,
-  SiTypescript: <SiTypescript />,
-  SiGithubcopilot: <SiGithubcopilot />,
-  SiNextdotjs: <SiNextdotjs />,
-  SiSass: <SiSass />
-};
+const skills = [
+  { name: "HTML", icon: <SiHtml5 />, link: "https://www.youtube.com/watch?v=qz0aGYrrlhU" },
+  { name: "CSS", icon: <SiCss3 />, link: "https://www.youtube.com/watch?v=OXGznpKZ_sA" },
+  { name: "JavaScript", icon: <SiJavascript />, link: "https://www.youtube.com/watch?v=EerdGm-ehJQ" },
+  { name: "React", icon: <SiReact />, link: "https://www.youtube.com/watch?v=SqcY0GlETPk" },
+  { name: "React Native", icon: <SiReact />, link: "https://www.youtube.com/watch?v=0-S5a0eXPoc" },
+  { name: "TypeScript", icon: <SiTypescript />, link: "https://youtu.be/d56mG7DezGs" },
+  { name: "Next.js", icon: <SiNextdotjs />, link: "https://www.youtube.com/watch?v=1WmNXEVia8g" },
+  { name: "Tailwind CSS", icon: <SiTailwindcss />, link: "https://www.youtube.com/watch?v=lCxcTsOHrjo" },
+  { name: "Sass", icon: <SiSass />, link: "https://www.youtube.com/watch?v=Zz6eOVaaelI" },
+  { name: "Bootstrap", icon: <SiBootstrap />, link: "https://www.youtube.com/watch?v=-qfEOE4vtxE" },
+  { name: "Git", icon: <SiGit />, link: "https://www.youtube.com/watch?v=zTjRZNkhiEU" },
+  { name: "Copilot", icon: <SiGithubcopilot />, link: "https://www.youtube.com/watch?v=JGLfyTDgfDc" },
+  { name: "Figma", icon: <SiFigma />, link: "https://www.youtube.com/watch?v=JGLfyTDgfDc" },
+  { name: "Photoshop", icon: <SiAdobephotoshop />, link: "https://www.youtube.com/watch?v=OjRqZiAgoHo" },
+  { name: "XD", icon: <SiAdobexd />, link: "https://www.youtube.com/watch?v=68w2VwalD5w" },
+  { name: "Illustrator", icon: <SiAdobeillustrator />, link: "https://www.youtube.com/watch?v=Ib8UBwu3yGA" }
+];
 
 const About = () => {
-  const skills = skillsData.map((s) => ({
-    name: s.name,
-    icon: iconMap[s.iconName] || <SiReact />,
-    link: s.link
-  }));
-
   return (
     <section
       id="about"
